0f245cb12751dbce1d0b41d291b2184ae989bbdf
seolmin <seolmin@megazone.com>
2023-10-13 18:34:55 +0900

    feat: add drop collection method of identity and dashboard services in v1.12.2

--- src/migration/v1_12_2.py
+++ src/migration/v1_12_2.py
@@ -7,60 +7,87 @@ from pymongo import UpdateOne
 _LOGGER = logging.getLogger(DEFAULT_LOGGER)
 
 
+# identity
+@print_log
+def identity_provider_drop(mongo_client: MongoCustomClient):
+    mongo_client.drop_collection('IDENTITY', 'provider')
+
+
+# dashboard
+@print_log
+def dashboard_domain_dashboard_drop(mongo_client: MongoCustomClient):
+    mongo_client.drop_collection('DASHBOARD', 'domain_dashboard')
+
+
+@print_log
+def dashboard_domain_dashboard_version_drop(mongo_client: MongoCustomClient):
+    mongo_client.drop_collection('DASHBOARD', 'domain_dashboard_version')
+
+
+@print_log
+def dashboard_project_dashboard_drop(mongo_client: MongoCustomClient):
+    mongo_client.drop_collection('DASHBOARD', 'project_dashboard')
+
+
+@print_log
+def dashboard_project_dashboard_version_drop(mongo_client: MongoCustomClient):
+    mongo_client.drop_collection('DASHBOARD', 'project_dashboard_version')
+
+
 # cost-analysis
 @print_log
 def cost_analysis_public_dashboard_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'public_dashboard')
+    mongo_client.drop_collection('COST_ANALYSIS', 'public_dashboard')
 
 
 @print_log
 def cost_analysis_user_dashboard_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'user_dashboard')
+    mongo_client.drop_collection('COST_ANALYSIS', 'user_dashboard')
 
 
 @print_log
 def cost_analysis_custom_widget_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'custom_widget')
+    mongo_client.drop_collection('COST_ANALYSIS', 'custom_widget')
 
 
 @print_log
 def cost_analysis_cost_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'cost')
+    mongo_client.drop_collection('COST_ANALYSIS', 'cost')
 
 
 @print_log
 def cost_analysis_monthly_cost_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'monthly_cost')
+    mongo_client.drop_collection('COST_ANALYSIS', 'monthly_cost')
 
 
 @print_log
 def cost_analysis_budget_usage_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'budget_usage')
+    mongo_client.drop_collection('COST_ANALYSIS', 'budget_usage')
 
 
 @print_log
 def cost_analysis_cost_query_set_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'cost_query_set')
+    mongo_client.drop_collection('COST_ANALYSIS', 'cost_query_set')
 
 
 @print_log
 def cost_analysis_cost_query_history_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'cost_query_history')
+    mongo_client.drop_collection('COST_ANALYSIS', 'cost_query_history')
 
 
 @print_log
 def cost_analysis_budget_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'budget')
+    mongo_client.drop_collection('COST_ANALYSIS', 'budget')
 
 
 @print_log
 def cost_analysis_job_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'job')
+    mongo_client.drop_collection('COST_ANALYSIS', 'job')
 
 
 @print_log
 def cost_analysis_job_task_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('COST-ANALYSIS', 'job_task')
+    mongo_client.drop_collection('COST_ANALYSIS', 'job_task')
 
 
 # inventory
@@ -108,6 +135,15 @@ def inventory_prowler_change_options_to_compliance_framework(mongo_client: Mongo
 
 def main(file_path):
     mongo_client: MongoCustomClient = MongoCustomClient(file_path, 'v1.12.2')
+
+    # identity
+    identity_provider_drop(mongo_client)
+
+    # dashboard
+    dashboard_domain_dashboard_drop(mongo_client)
+    dashboard_domain_dashboard_version_drop(mongo_client)
+    dashboard_project_dashboard_drop(mongo_client)
+    dashboard_project_dashboard_version_drop(mongo_client)
 
     # cost-analysis
     cost_analysis_public_dashboard_drop(mongo_client)
