4bd798943b644f2b7509ee0fca1e6e003f8c87c9
mz-ko <hyupko@megazone.com>
2024-01-04 01:13:55 +0900

    fix: change plugin to new style

--- src/migration/v2_0_1/plugin.py
+++ src/migration/v2_0_1/plugin.py
@@ -5,24 +5,10 @@ from lib.util import print_log
 
 _LOGGER = logging.getLogger(DEFAULT_LOGGER)
 
-@print_log
-def plugin_installed_plugin_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('PLUGIN', 'installed_plugin')
-
 
 @print_log
-def plugin_installed_plugin_ref_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('PLUGIN', 'installed_plugin_ref')
-
-
-@print_log
-def plugin_supervisor_drop(mongo_client: MongoCustomClient):
-    mongo_client.drop_collection('PLUGIN', 'supervisor')
-
-
-def main(file_path):
-    mongo_client: MongoCustomClient = MongoCustomClient(file_path, 'v2.0.0')
-
-    plugin_installed_plugin_drop(mongo_client)
-    plugin_installed_plugin_ref_drop(mongo_client)
-    plugin_supervisor_drop(mongo_client)
+def drop_collections(mongo_client):
+    # drop role after refactoring role_binding
+    collections = ["installed_plugin", "installed_plugin_ref", "supervisor"]
+    for collection in collections:
+        mongo_client.drop_collection('PLUGIN', collection)
